89b1a5615d0c8192eb6520c8b4d38fff14e134c0
Mohsin Ali Zaidi <web20@toppagerankers.com>
2025-12-03 14:30:05 -0800

    utils code discussion

--- src/index.js
+++ src/index.js
@@ -7,3 +7,18 @@ dotenv.config({
 
 
 connectDB()
+.then(() => {
+    app.on("error", (error) => {
+        console.log("ERROR", error);
+        
+    })
+
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port: ${process.env.PORT}`);
+        
+    })
+})
+.catch((err) => {
+    console.log("MongoDB connection failed !!! ", err);
+    
+})
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,32 @@
+const asyncHandler = (requesHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requesHandler(req, res, next))
+        .catch((err) => next(err))
+    }
+}
+
+
+
+export {asyncHandler}
+
+
+
+
+
+
+
+
+
+
+
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try {
+//         await fn(req, res, next)
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success: false,
+//             message: error.message
+//         })
+//     }
+// }

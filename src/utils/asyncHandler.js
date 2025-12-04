59e75eba433a82942abf8ab51274da680865013b
Mohsin Ali Zaidi <web20@toppagerankers.com>
2025-12-04 10:28:49 -0800

    Add user and video models

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,13 +1,14 @@
 const asyncHandler = (requesHandler) => {
-    (req, res, next) => {
+    return (req, res, next) => {
         Promise.resolve(requesHandler(req, res, next))
         .catch((err) => next(err))
     }
 }
 
 
-
 export {asyncHandler}
+
+
 
 
 

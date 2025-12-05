c3d43e76f4fc70f491ea761b1aa36c64c2c2d7e7
Mohsin Ali Zaidi <web20@toppagerankers.com>
2025-12-05 11:28:14 -0800

    All setup configured

--- src/app.js
+++ src/app.js
@@ -24,4 +24,15 @@ app.use(cookieParser());
 
 
 
+//routes import
+
+import userRouter from "./routes/user.routes.js"
+
+
+//routes declaration
+
+app.use("/api/v1/users", userRouter)
+
+
+
 export { app }
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -50,7 +50,7 @@ const userSchema = new Schema({
 userSchema.pre("save", async function (next) {
     if(!this.isModified("password")) return next();
     
-    this.password = bcrypt.hash(this.password, 10)
+    this.password = await bcrypt.hash(this.password, 10)
     next()
 })
 
@@ -63,7 +63,7 @@ userSchema.methods.generateAccessToken = function(){
         {
         _id: this._id,
         email: this.email,
-        username: thiis.username,
+        username: this.username,
         fullName: this.fullName
         },
         process.env.ACCESS_TOKEN_SECRET,
